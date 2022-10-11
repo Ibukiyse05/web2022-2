a33ef752b82e72f141a8410a206428d2ab266830
Ibukiyse05 <103973942+Ibukiyse05@users.noreply.github.com>
2022-10-11 10:11:07 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -1,3 +1,10 @@
-ver who65=world;
+var who65 = 'world';
 function sayhello65(){
-  alert(Hello,
+alert('Hello,'+who65+'!');
+}
+function taro65() 
+{who65 = '太郎';
+}
+unction hanako65(){
+who65 ='花子';
+}
