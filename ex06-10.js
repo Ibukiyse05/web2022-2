46cd3c6d1f01db07317b303a3a1dd55c56c4585d
Ibukiyse05 <103973942+Ibukiyse05@users.noreply.github.com>
2022-11-22 10:19:47 +0900

    Update ex06-10.js

--- ex06-10.js
+++ ex06-10.js
@@ -1,9 +1,9 @@
 function imgchange1() 
 {
-document.getElementById('logo').src='./image/bulbon.gif';
+document.getElementById('logo').src='./image/denkyuu.png';
 }
 
 function imgchange2()
 {
-document.getElementById('logo').src='./image/josai.png';
+document.getElementById('logo').src='./image/denkyuu_on.png';
 }
