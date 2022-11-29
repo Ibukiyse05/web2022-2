47d14e501e592abc701f94b75b8157f1517bf3db
Ibukiyse05 <103973942+Ibukiyse05@users.noreply.github.com>
2022-11-29 10:28:39 +0900

    Update ex06-11.js

--- ex06-11.js
+++ ex06-11.js
@@ -1,24 +1,14 @@
-function hyouji() {
-//表示する文字  
-var str="一文字すつ表示します。";
+function hyoji() { var str = "一文字ずつ表示します。"; 
+var cnt = document.timer.moji.value.length; 
+if ( cnt < 11 ) { document.timer.moji.value = str.substr(0,cnt+1); } 
+else { document.timer.moji.value = ""; } } 
+function startfnc() { setInterval("hyoji()",1000); }
 
-//テキストボックスの文字数
-var cnt=document.timer.moji.value.length;
+function mes() { alert("3秒経ちました！"); }
 
-//文字が全部表示されているか確認
-if(cnt<11) {
-
-//現在より1文字多く切り出して表示
-document.timer.moji.value=str.substr(0,cnt+1); }
-  
-else {
-//全て表示されたら、空文字に戻す
-document.timer.moji.value=""; } }
-
-function startfnc() {
-//関数hyouji()を1000ミリ秒間隔で呼び出す
-setInterval("hyouji()",1000); }
-
-function mes() {
-alert("3秒経ちました!"); }
-
+function hyoji2() { var str = "一文字ずつ表示します。"; 
+var cnt = document.timer2.moji.value.length; 
+if ( cnt < 11 ) { document.timer2.moji.value = str.substr(0,cnt+1); } 
+else { document.timer2.moji.value = ""; } 
+setTimeout("hyoji2()",1000); }
+function startfnc2() { setTimeout("hyoji2()",1000); }
