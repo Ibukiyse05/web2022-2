9b99c802cf7c5e2d84ab74869a6741ebea699b6f
Ibukiyse05 <103973942+Ibukiyse05@users.noreply.github.com>
2023-01-17 10:04:08 +0900

    Update ex06-88.js

--- ex06-88.js
+++ ex06-88.js
@@ -1,1 +1,1 @@
-function imgchange688(){document.getElementById('josai').innerHTML='<img src="./image/josai.png" alt="オン">';}
+function imgchange688(){document.getElementById('josai').innerHTML='<img src="./image/josai.jpg" alt="オン">';}
